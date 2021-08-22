c3081a256761631d437ecc40d4770e9a260596ac
Barry <btarlton@gmail.com>
2021-08-22 17:23:26 -0400

    Update topic_checker.py to loop continually and launch consumer.py

--- topic_checker.py
+++ topic_checker.py
@@ -1,4 +1,7 @@
 import subprocess
+import sys
+import time
+
 
 def get_partition_and_leader(topic_string):
     partitionIndex = topic_string.find("Partition:")
@@ -8,27 +11,62 @@ def get_partition_and_leader(topic_string):
     leaderIndex = topic_string.find("Leader:")
     leaderValue = "-1"
     if partitionIndex > -1:
-        partitionIndex+=10
-        partitionValue = topic_string[partitionIndex:partitionIndex+3].strip()
+        partitionIndex += 10
+        partitionValue = topic_string[partitionIndex:partitionIndex + 3].strip()
     if partitionIndex > -1:
-        leaderIndex+=7
-        leaderValue = topic_string[leaderIndex:leaderIndex+3].strip()
-
+        leaderIndex += 7
+        leaderValue = topic_string[leaderIndex:leaderIndex + 3].strip()
 
     return {"partition": partitionValue, "leader": leaderValue}
 
-process = subprocess.run(["/opt/kafka/kafka_2.13-2.8.0/bin/kafka-topics.sh", "--bootstrap-server", "192.168.1.120:9092","--topic", "barry","--describe"],capture_output=True,text=True)
-#process = subprocess.run(["/opt/kafka/kafka_2.13-2.8.0/bin/kafka-topics.sh", "--bootstrap-server", "192.168.1.120:9092","--topic", "barry","--describe"],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-print("return code" + str(process.returncode))
-print("stdout=")
-#print(process.stdout)
-lines = process.stdout.splitlines()
-for line in lines:
-  print("line:" + line)
-  part_leader = get_partition_and_leader(line)
-  partition = part_leader.get("partition")
-  leader = part_leader.get("leader")
-  if(leader=="0"):
-    print("leader is here!!!!!!!!!!!!")
-    print("partition to watch:" + partition)
+
+def launch_consumer(partition):
+    print("launching consumer for partition:" + partition)
+    consumer_process = subprocess.Popen(["sudo", "python3", "/home/pi/consumer.py", partition],stdout=log_file, stderr=subprocess.STDOUT)
+    return consumer_process
+
+def open_log_file(logfilepath):
+    return open(logfilepath, mode='w')
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        raise ValueError('Provide path to kafka install as argument')
+    kafka_path = sys.argv[1]
+    kafka_topic_command = kafka_path + "bin/kafka-topics.sh"
+    print("Kafka topic command =" + kafka_topic_command)
+    consumer_process_launched = False
+    partition_found_here = False
+    log_file = open_log_file("/home/pi/consumer.log")
+
+    while True:
+        # start checking if kafka topic partition is on this leader.
+        process = subprocess.run([kafka_topic_command, "--bootstrap-server", "192.168.1.120:9092",
+                                  "--topic", "barry", "--describe"], capture_output=True, text=True)
+
+        print("return code" + str(process.returncode))
+        print("stdout=")
+        # print(process.stdout)
+        lines = process.stdout.splitlines()
+        for line in lines:
+            print("line:" + line)
+            part_leader = get_partition_and_leader(line)
+            partition = part_leader.get("partition")
+            leader = part_leader.get("leader")
+            if (leader == "0"):
+                print("leader is here!!!!!!!!!!!!")
+                print("partition to watch:" + partition)
+                partition_found_here = True
+                break
+            else:
+                partition_found_here = False
+        if partition_found_here:
+            if not consumer_process_launched:
+                consumer_process_to_watch = launch_consumer(partition)
+                consumer_process_launched = True
+        elif consumer_process_launched:
+            consumer_process_to_watch.kill()
+            consumer_process_launched = False
+        time.sleep(1) # wait 1 sec before checking again
+
 
