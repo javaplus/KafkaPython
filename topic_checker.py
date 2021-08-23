77235fdf278789789a5add3b55db6657b9c60bec
Barry <btarlton@gmail.com>
2021-08-23 16:26:48 -0400

    Update topic_checker.py
    minor fixes

--- topic_checker.py
+++ topic_checker.py
@@ -3,6 +3,8 @@ import sys
 import time
 import os
 
+topic_name = "colors"
+
 def get_partition_and_leader(topic_string):
     partitionIndex = topic_string.find("Partition:")
     partitionValue = "-1"
@@ -22,7 +24,7 @@ def get_partition_and_leader(topic_string):
 
 def launch_consumer(partition):
     print("launching consumer for partition:" + partition)
-    consumer_process = subprocess.Popen(["sudo", "python3", "/home/pi/consumer.py", partition],stdout=log_file, stderr=subprocess.STDOUT)
+    consumer_process = subprocess.Popen(["python3", "/home/pi/consumer.py", partition],stdout=log_file, stderr=subprocess.STDOUT)
     return consumer_process
 
 def open_log_file(logfilepath):
@@ -34,6 +36,8 @@ def get_kafka_home():
     return kafka_home
 
 if __name__ == '__main__':
+
+    my_leader = "0"
 
     kafka_home = get_kafka_home()
     if not kafka_home:
@@ -47,7 +51,7 @@ if __name__ == '__main__':
     while True:
         # start checking if kafka topic partition is on this leader.
         process = subprocess.run([kafka_topic_command, "--bootstrap-server", "192.168.1.120:9092",
-                                  "--topic", "barry", "--describe"], capture_output=True, text=True)
+                                  "--topic", topic_name, "--describe"], capture_output=True, text=True)
 
         print("return code" + str(process.returncode))
         print("stdout=")
@@ -58,7 +62,7 @@ if __name__ == '__main__':
             part_leader = get_partition_and_leader(line)
             partition = part_leader.get("partition")
             leader = part_leader.get("leader")
-            if (leader == "0"):
+            if (leader == my_leader):
                 print("leader is here!!!!!!!!!!!!")
                 print("partition to watch:" + partition)
                 partition_found_here = True
@@ -70,6 +74,7 @@ if __name__ == '__main__':
                 consumer_process_to_watch = launch_consumer(partition)
                 consumer_process_launched = True
         elif consumer_process_launched:
+            consumer_process_to_watch.terminate()
             consumer_process_to_watch.kill()
             consumer_process_launched = False
         time.sleep(1) # wait 1 sec before checking again
