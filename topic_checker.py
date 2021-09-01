277a22d853fa6d9573674ac5fa20588e8eea8556
Barry <btarlton@gmail.com>
2021-09-01 15:11:00 -0400

    Latest update before constant checking

--- topic_checker.py
+++ topic_checker.py
@@ -2,14 +2,19 @@ import subprocess
 import sys
 import time
 import os
+import logging
 
+logging.basicConfig(filename='/home/pi/logs/topic_checker.log', level=logging.DEBUG, filemode='w', format='%(asctime)s %(message)s')
 topic_name = "colors"
+
+def logMessage(message):
+    logging.info(message)
 
 def get_partition_and_leader(topic_string):
     partitionIndex = topic_string.find("Partition:")
     partitionValue = "-1"
-    print("PartitionIndex=")
-    print(partitionIndex)
+    logMessage("PartitionIndex=")
+    logMessage(partitionIndex)
     leaderIndex = topic_string.find("Leader:")
     leaderValue = "-1"
     if partitionIndex > -1:
@@ -23,7 +28,7 @@ def get_partition_and_leader(topic_string):
 
 
 def launch_consumer(partition):
-    print("launching consumer for partition:" + partition)
+    logMessage("launching consumer for partition:" + partition)
     consumer_process = subprocess.Popen(["python3", "/home/pi/consumer.py", partition],stdout=log_file, stderr=subprocess.STDOUT)
     return consumer_process
 
@@ -32,7 +37,7 @@ def open_log_file(logfilepath):
 
 def get_kafka_home():
     kafka_home = os.environ['KAFKA_HOME']
-    print(kafka_home)
+    logMessage(kafka_home)
     return kafka_home
 
 if __name__ == '__main__':
@@ -43,28 +48,28 @@ if __name__ == '__main__':
     if not kafka_home:
         raise ValueError('Provide path to kafka install as argument')
     kafka_topic_command = kafka_home + "bin/kafka-topics.sh"
-    print("Kafka topic command =" + kafka_topic_command)
+    logMessage("Kafka topic command =" + kafka_topic_command)
     consumer_process_launched = False
     partition_found_here = False
     log_file = open_log_file("/home/pi/consumer.log")
-
+    
     while True:
         # start checking if kafka topic partition is on this leader.
-        process = subprocess.run([kafka_topic_command, "--bootstrap-server", "192.168.1.120:9092",
+        process = subprocess.run([kafka_topic_command, "--bootstrap-server", "192.168.8.10:9092,192.168.8.20:9092,192.168.8.30:9092",
                                   "--topic", topic_name, "--describe"], capture_output=True, text=True)
 
-        print("return code" + str(process.returncode))
-        print("stdout=")
-        # print(process.stdout)
+        logMessage("return code" + str(process.returncode))
+        logMessage("stdout=")
+        # logMessage(process.stdout)
         lines = process.stdout.splitlines()
         for line in lines:
-            print("line:" + line)
+            logMessage("line:" + line)
             part_leader = get_partition_and_leader(line)
             partition = part_leader.get("partition")
             leader = part_leader.get("leader")
             if (leader == my_leader):
-                print("leader is here!!!!!!!!!!!!")
-                print("partition to watch:" + partition)
+                logMessage("leader is here!!!!!!!!!!!!")
+                logMessage("partition to watch:" + partition)
                 partition_found_here = True
                 break
             else:
