f314bfd2d8c845ed76153b9e163628788940b6c2
Barry <btarlton@gmail.com>
2021-09-10 20:05:05 -0400

    Create producer.py

---
+++ producer.py
@@ -0,0 +1,53 @@
+from confluent_kafka import Producer
+import socket
+import time
+import logging
+
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+
+logging.basicConfig(filename='/home/pi/logs/producer.log', level=logging.DEBUG, filemode='w', format='%(asctime)s %(message)s')
+mylogger = logging.getLogger()
+
+def logMessage(message):
+    logging.info(message)
+
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 8 to be an input pin and set
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 5 to be an input pin and set
+
+delay = .15
+
+# conf = {'bootstrap.servers': '192.168.8.10:9092,192.168.8.20:9092,192.168.8.30:9092',
+conf = {'bootstrap.servers': '192.168.8.10:9092,192.168.8.20:9092',
+        'client.id': 'piProducer1',
+        'metadata.max.age.ms':'20000'}
+topic = 'colors'
+producer = Producer(conf,logger=mylogger)
+
+def produceMessage(message):
+    if message == "red":
+      producer.produce(topic, key="mymessageisred", value=message)
+    else:
+      producer.produce(topic, key=message, value=message)
+    producer.flush()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    logMessage('Starting main')
+    while True:
+      if GPIO.input(10) == GPIO.HIGH:
+        produceMessage("red")
+        logMessage('Sending Red')
+        time.sleep(delay)
+      if GPIO.input(8) == GPIO.HIGH:
+        produceMessage("blue")
+        logMessage('Sending blue')
+        time.sleep(delay)
+      if GPIO.input(16) == GPIO.HIGH:
+        produceMessage("yellow")
+        logMessage('Sending yeloo')
+        time.sleep(delay)
