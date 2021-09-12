71c98a222998ea010073e58e1b1b02bcb9e52029
Barry <btarlton@gmail.com>
2021-09-11 22:44:32 -0400

    update to get latest updates to admin

--- admin.py
+++ admin.py
@@ -1,9 +1,38 @@
 from confluent_kafka.admin import AdminClient, NewTopic, NewPartitions
+import time
+import board
+import logging
+import time
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+
+BUTTON_ONE = 22
+BUTTON_TWO = 27
+GPIO.setwarnings(False) # Ignore warning for now
+#GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(BUTTON_ONE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_TWO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+logging.basicConfig(filename='/home/pi/logs/admin.log', level=logging.DEBUG, filemode='w', format='%(asctime)s %(message)s')
+mylogger = logging.getLogger()
+
+
+def logMessage(message):
+    logging.info(message)
+    # print(message)
+
 
 a = AdminClient({'bootstrap.servers': '192.168.8.10:9092'})
 topic_name = "colors"
 
-partitions = NewPartitions("test", 1, )
+number_of_partitions = 0
+
+
+def checkButton(button):
+    if GPIO.input(button) == GPIO.HIGH:
+        return True
+    else:
+        return False
 
 
 def create_second_partition(a):
@@ -15,11 +44,11 @@ def create_second_partition(a):
     for topic, f in fs.items():
         try:
             f.result()  # The result itself is None
-            print("Additional partitions created for topic {}".format(topic))
+            logMessage("Additional partitions created for topic {}".format(topic))
         except Exception as e:
-            print("Failed to add partitions to topic {}: {}".format(topic, e))
+            logMessage("Failed to add partitions to topic {}: {}".format(topic, e))
 
-
+            
 def create_third_partition(a):
     """ create partitions """
     new_parts = [NewPartitions(topic_name, 3, [[2, 0]])]
@@ -29,9 +58,9 @@ def create_third_partition(a):
     for topic, f in fs.items():
         try:
             f.result()  # The result itself is None
-            print("Additional partitions created for topic {}".format(topic))
+            logMessage("Additional partitions created for topic {}".format(topic))
         except Exception as e:
-            print("Failed to add partitions to topic {}: {}".format(topic, e))
+            logMessage("Failed to add partitions to topic {}: {}".format(topic, e))
 
 
 def delete_topic(a, topic):
@@ -42,16 +71,13 @@ def delete_topic(a, topic):
     for topic, f in fs.items():
         try:
             f.result()  # The result itself is None
-            print("Topic {} deleted".format(topic))
+            logMessage("Topic {} deleted".format(topic))
         except Exception as e:
-            print("Failed to delete topic {}: {}".format(topic, e))
-
-
-
+            logMessage("Failed to delete topic {}: {}".format(topic, e))
 
 
 def create_topic(a):
-    new_topics = [NewTopic(topic_name, num_partitions=1, replication_factor=2)]
+    new_topics = [NewTopic(topic_name, num_partitions=1, replication_factor=-1, replica_assignment=[[0, 1]])]
     # Note: In a multi-cluster production scenario, it is more typical to use a replication_factor of 3 for durability.
     # Call create_topics to asynchronously create topics. A dict
     # of <topic,future> is returned.
@@ -60,23 +86,25 @@ def create_topic(a):
     for topic, f in fs.items():
         try:
             f.result()  # The result itself is None
-            print("Topic {} created".format(topic))
+            logMessage("Topic {} created".format(topic))
         except Exception as e:
-            print("Failed to create topic {}: {}".format(topic, e))
+            logMessage("Failed to create topic {}: {}".format(topic, e))
 
-def test(topics):
 
-    myTopic = topics[0::2]
-    print(myTopic)
-    myTopic2 = topics[1::2]
-    print(myTopic2)
-    for topic, new_total_count in zip(topics[0::2], topics[1::2]):
-        print(topic)
-        print(new_total_count)
+while True:
+    button1Down = checkButton(BUTTON_ONE)
+    button2Down = checkButton(BUTTON_TWO)
+    if number_of_partitions == 1 and button1Down:
+        create_second_partition(a)
+        number_of_partitions = 2
+    elif number_of_partitions == 2 and button2Down:
+        create_third_partition(a)
+        number_of_partitions = 3
+    elif number_of_partitions != 1 and not button1Down and not button2Down:
+        delete_topic(a, topic_name)
+        number_of_partitions = 1
+        time.sleep(3)
+        create_topic(a)
+    time.sleep(3)
 
-#test(["test","1","newtopic","2"])
 
-#delete_topics(a,["test"])
-#create_topic()
-#topics = ["test"]
-create_second_partition(a)
