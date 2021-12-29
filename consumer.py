864a5d01107b7739131d8db640de9bdb05cfc62c
Barry <btarlton@gmail.com>
2021-12-28 20:30:06 -0500

    consumer.py use RGB values received directly

--- consumer.py
+++ consumer.py
@@ -1,108 +1,85 @@
 import board
 import neopixel
-from confluent_kafka import Consumer, KafkaError
+import sys
+from confluent_kafka import Consumer, KafkaError, TopicPartition
 import logging
-import time
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
-#GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-# Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+import signal,sys
+
 
 logging.basicConfig(filename='/home/pi/logs/consumer.log', level=logging.DEBUG, filemode='w', format='%(asctime)s %(message)s')
-mylogger = logging.getLogger()
+
 def logMessage(message):
     logging.info(message)
 
+topic_name = "colors"
+
+if len(sys.argv)!=2:
+  raise ValueError('Provide Partition as argument')
 num_of_leds = 10
-num_of_plaque_leds = 23
 red = (255,0,0)
 blue = (0,0,255)
 green = (0,255,0)
 yellow  = (255,255,0)
 
-current_index=0
+pixels = neopixel.NeoPixel(board.D18, num_of_leds,brightness=0.2)
 
 def clear_bar():
     for x in range(num_of_leds):
         pixels[x] = (0, 0, 0)
-    current_index=0
+def indicate_partition(partition_number):
+    logMessage("in indicate_partition:"+str(partition_number))
+    myrange=range(int(partition_number)+1)
+    for index in myrange:
+        pixels[index] = green
+        logMessage("making indexGreen:" + str(index))
 
-pixels = neopixel.NeoPixel(board.D18, num_of_leds + num_of_plaque_leds)
+def termination_handler(signum, frame):
+  logMessage("I'm shutting down vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvVVVVVVVVVVVVVVVVV")
+  clear_bar()
+  sys.exit(0)
+
+signal.signal(signal.SIGTERM, termination_handler)
+partition = sys.argv[1]
+
 clear_bar()
+indicate_partition(partition)
 
-
+current_index=0
 
 settings = {
     'bootstrap.servers': '192.168.8.10:9092,192.168.8.20:9092,192.168.8.30:9092',
-    'group.id': 'pie',
-    'client.id': 'pie-1',
+    'group.id': 'broker0',
+    'client.id': 'pi-broker0',
     'enable.auto.commit': True,
     'session.timeout.ms': 6000,
-    'metadata.max.age.ms': 20000,
     'default.topic.config': {'auto.offset.reset': 'smallest'}
 }
-button_on = True
 
-
-def checkButton():
-    if GPIO.input(21) == GPIO.HIGH:
-        return True
-    else:
-        return False
-
-def lightNamePlaque():
-    totalLeds = num_of_leds + num_of_plaque_leds
-    for pixel in range(num_of_leds, totalLeds):
-        pixels[pixel] = (255,255,255)
-
-def turnOffNamePlaque():
-    totalLeds = num_of_leds + num_of_plaque_leds
-    for pixel in range(num_of_leds,totalLeds):
-        pixels[pixel] = (0,0,0)
+logMessage("partition:" + partition)
+c = Consumer(settings)
+c.assign([TopicPartition(topic_name, int(partition))])
+# c.subscribe(['barry'])
 try:
     while True:
-        button_on = checkButton()
-        if button_on:
-            c = Consumer(settings, logger=mylogger)
-            c.subscribe(['colors'])
-            logMessage("Subscribing to Topic")
-            lightNamePlaque()
+        msg = c.poll(0.1)
+        if msg is None:
+            continue
+        elif not msg.error():
+            msgvalue = msg.value().decode('utf-8')
+            logMessage('Received message: {0}'.format(msgvalue))
+            logMessage("Hello from here")
+            if (current_index >= num_of_leds):
+                clear_bar()
+                current_index = 0
+            pixels[current_index] = tuple(map(int,msgvalue.split(",")))
+            current_index = current_index + 1
+            logMessage("Doned!!!!!")
+        elif msg.error().code() == KafkaError._PARTITION_EOF:
+            logMessage('End of partition reached {0}/{1}'
+                  .format(msg.topic(), msg.partition()))
+        else:
+            logMessage('Error occured: {0}'.format(msg.error().str()))
 
-            while button_on:
-                button_on = checkButton()
-                msg = c.poll(0.1)
-                if msg is None:
-                    continue
-                elif not msg.error():
-                    msgvalue= msg.value().decode('utf-8')
-                    logMessage('Received message: {0}'.format(msgvalue))
-                    logMessage("Hello from here")
-                    if(current_index>=num_of_leds):
-                        clear_bar()
-                        current_index=0
-                    if(msgvalue=='red'):
-                        pixels[current_index] = red
-                    if(msgvalue=='blue'):
-                        pixels[current_index] = blue
-                    if (msgvalue == 'green'):
-                        pixels[current_index] = green
-                    if (msgvalue == 'yellow'):
-                        pixels[current_index] = yellow
-                    current_index = current_index + 1
-                    logMessage("Doned!!!!!")
-                elif msg.error().code() == KafkaError._PARTITION_EOF:
-                    logMessage('End of partition reached {0}/{1}'
-                          .format(msg.topic(), msg.partition()))
-                else:
-                    logMessage('Error occured: {0}'.format(msg.error().str()))
-
-            logMessage("Closing consumer")
-            clear_bar()
-            turnOffNamePlaque()
-            current_index=0
-            c.close()
-        time.sleep(.15)
 except KeyboardInterrupt:
     pass
 
